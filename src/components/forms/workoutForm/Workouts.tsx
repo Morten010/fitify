8f83b9b3157f91d46b5c6dae52d1d2ff17a9efb4
Morten010 <97233551+Morten010@users.noreply.github.com>
2023-09-27 16:05:52 +0200

    Update Workouts.tsx

--- src/components/forms/workoutForm/Workouts.tsx
+++ src/components/forms/workoutForm/Workouts.tsx
@@ -14,6 +14,7 @@ type WorkoutsProps = {
     workoutState: {
         title: string;
         description: string;
+        public: boolean;
         days: DayProps[];
     },
     workoutDispatch: React.Dispatch<FormProps>,
