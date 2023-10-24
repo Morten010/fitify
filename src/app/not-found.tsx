ea654c0c3d41a74b913be5463e49c726dc052353
Morten010 <97233551+Morten010@users.noreply.github.com>
2023-10-24 10:26:23 +0200

    not found pae added

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,24 @@
+import Image from 'next/image'
 import React from 'react'
 
-export default function notFound() {
+export default function offline() {
+
   return (
-    <div>
-        Not found
+    <div
+    className='max-w-screen-lg mx-auto pb-[68px] h-screen flex flex-col justify-center'
+    >
+      <Image 
+      src="/not-found.svg"
+      alt='No connection'
+      width={300}
+      height={242}
+      className='mx-auto'
+      />
+      <h1
+      className='text-center class text-muted-foreground'
+      >
+        page not found
+      </h1>
     </div>
   )
 }
