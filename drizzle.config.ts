a0456707b64ff3d7add8c80af1eff1cd6e316e5d
Morten010 <97233551+Morten010@users.noreply.github.com>
2025-03-08 18:50:57 +0100

    updated db config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,14 @@
 import type { Config } from "drizzle-kit";
 
 if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is missing');
+  throw new Error("DATABASE_URL is missing");
 }
 
 export default {
   schema: "./src/db/schema.ts",
   out: "./drizzle",
-  driver: "mysql2",
+  dialect: "mysql",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL,
-  }
+    url: process.env.DATABASE_URL,
+  },
 } satisfies Config;
